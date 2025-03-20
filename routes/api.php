dfe6b93df3b8532dcb50970a13b826594e066779
hichamoubaha <oubahahicham7@gmail.com>
2025-03-20 12:09:58 +0000

    add the routes of statut update

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-
 use App\Http\Controllers\CandidatureController;
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('candidatures/{id}/statut', [CandidatureController::class, 'mettreAJourStatut']);
+});
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/candidatures', [CandidatureController::class, 'postuler']);
     Route::delete('/candidatures/{id}', [CandidatureController::class, 'retirer']);
     Route::get('/recruteur/candidatures', [CandidatureController::class, 'listeRecruteur']);
 });
+
+
 
 use App\Http\Controllers\OffreController;
 Route::middleware(['auth:sanctum'])->group(function () {
